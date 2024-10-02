2dde5abf80df315f670cac8a09d5e517f61950db
SamvanCrimpen123 <sam.van.crimpen@hva.nl>
2024-10-02 10:09:16 +0200

    css js en menu gefixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,10 @@
-// JavaScript Document
-console.log("hi");
+const menuButton = document.querySelector("header button");
+const hetMenu = document.querySelector("header nav");
+
+function toggleMenu() {
+	hetMenu.classList.toggle("open");
+}
+
+menuButton.onclick = toggleMenu;
+
+console.log("hallo")
