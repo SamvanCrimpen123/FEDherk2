5de0e42b953be6ce86ca86b0643682dc0dde6c60
SamvanCrimpen123 <sam.van.crimpen@hva.nl>
2024-11-18 09:38:32 +0100

    header

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,19 @@
 const menuButton = document.querySelector("header button");
 const hetMenu = document.querySelector("header nav section:first-of-type");
-
+menuButton.onclick = toggleMenu;
 function toggleMenu() {
 	hetMenu.classList.toggle("open");
 }
 
-menuButton.onclick = toggleMenu;
 
-console.log("hallo")
+
+const navButton = document.querySelector("header nav section:nth-of-type(1) ul li:nth-of-type(1) img");
+const hetNav = document.querySelector("header nav section:first-of-type");
+navButton.onclick = sluitNav;
+
+function sluitNav() {
+	hetNav.classList.remove("open");
+}
+
+
+
